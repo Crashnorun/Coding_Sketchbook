14fc911d6473b02cfd405b6c592efb8b4267965e
Crashnorun <charlie.portelli@gmail.com>
2018-05-14 19:56:49 -0400

    added d3 js file for intellisense

--- D3/BarChart/main.js
+++ D3/BarChart/main.js
@@ -1,19 +1,18 @@
+/**
+ * By adding the non minified D3 file into the project directory
+ * adds intellisense to this document.
+ */
 
-var data = [4, 8, 15, 16, 23, 42];
+var svg = d3.select("svg");
+var margin = { top: 20, right: 20, bottom: 20, left: 20 },
+    width = + svg.attr("width") - margin.left - margin.right,
+    height = + svg.attr("height") - margin.top - margin.bottom;
 
-// var svg = d3.select("svg"),
-//     margin = { top: 20, right: 20, bottom: 20, left: 20 },
-//     width = + svg.attr("width") - margin.left - margin.right,
-//     height = + svg.attr("height") - margin.top - margin.bottom,
-//     g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+var formatPercent = d3.format(".0%");
 
-// svg.data(data);
+var x = d3.scaleOrdinal([0, width]);
+var y = d3.scaleLinear([height, 0]);
 
+var xAxis = d3.svg.scaleOrdinal; //(x.range).orient.bottom();
 
-
-var chart = d3.select(".chart")
-    .selectAll("div")
-    .data(data)
-    .enter().append("div")
-    .style("width", function (d) { return d * 10 + "px"; })
-    .text(function (d) { return d; });
+console.log(xAxis);
