7d6610c22c965022277df31ef2e1c3e5df7137c8
Crashnorun <charlie.portelli@gmail.com>
2018-05-27 19:22:20 -0400

    getting closer

--- D3/BarChart/main.js
+++ D3/BarChart/main.js
@@ -27,22 +27,49 @@ svg = d3.select("body").append("svg")
 svg.call(tip);
 
 var file = "https://raw.githubusercontent.com/Crashnorun/Coding_Sketchbook/master/D3/BarChart/data.tsv";
-d3.tsv(file, type, readData(file.data));
+//d3.tsv(file, type, readData(file.data));
 
-function readData(error, data) {
-    x.domain(data.map(returnLetter));
-    y.domain([0, d3.max(returnFrequency)]);
+d3.tsv(file, readData(file));
+
+function readData(data) {
+    x.domain(d3.extent(data, function (d) { return d.letter; }));
+    y.domain(d3.extent(data, function (d) { return d.frequency; }));
+    //x.domain(data.map(returnLetter));
+    //y.domain([0, d3.max(returnFrequency)]);
 }
 
-function returnFrequency(d) {
-    return d.frequency;
-}
+function returnFrequency(d) { return d.frequency; }
 
-function returnLetter(d) {
-    return d.letter;
-}
+function returnLetter(d) { return d.letter; }
 
 function type(d) {
     d.frequency = +d.frequency;
     return d;
 }
+
+
+svg.append("g")
+    .attr("class", "x axis")
+    .attr("transform", "translte(0," + height + ")")
+    .call(xAxis);
+
+svg.append("g")
+    .attr("class", "y axis")
+    .call(yAxis)
+    .append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 6)
+    .attr("dy", ".71em")
+    .style("text-anchor", "end")
+    .text("Frequency");
+
+svg.selectAll(".bar")
+    .data(file)
+    .enter().append("rect")
+    .attr("class", "bar")
+    .attr("x", function (d) { return x(d.letter); })
+    .attr("width", x.range())
+    .attr("y", function (d) { return y(d.frequency); })
+    .attr("height", function (d) { return height - y(d.frequence); })
+    .on('mouseover', tip.show)
+    .on('mouseout', tip.hide);
