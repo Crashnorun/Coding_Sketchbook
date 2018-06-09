22b71f25d2227ed0125f0cb5f97dbd6fe975262f
Crashnorun <charlie.portelli@gmail.com>
2018-06-09 10:26:38 -0400

    Tighter integration of radius variable.

--- P5/Arcs_01/main.js
+++ P5/Arcs_01/main.js
@@ -7,32 +7,35 @@ function setup() {
     stroke(0);
     strokeWeight(1);
     noFill();
-//ellipse(width / 2, height / 2, 600, 600);
+    //ellipse(width / 2, height / 2, 600, 600);
 
     let startAng = PI;
     let endAng = PI * 2;
-    let startRad = 20;
+    let startRad =50;
+    let deltaRad = 2;
+    let rad = startRad;
 
-    // for (let i = -10 + startRad; i < 300; i += 10) {
-    //     let b = map(i, -10, width / 2, 50, 200);
+    // for (let i = -startRad + startRad; i < 300; i += deltaRad) {
+    //     let b = map(i, -deltaRad, width / 2, 50, 200);
     //     stroke(b, b, 255);
     //     arc(i, height / 2, startRad, startRad, startAng, endAng);
 
-    //     b = map(i, width / 2, -10, 50, 200);
+    //     b = map(i, width / 2, -deltaRad, 50, 200);
     //     stroke(b, b, 255);
     //     arc(width / 2 - i, height / 2, startRad - 600, startRad - 600, endAng, startAng);
-    //     startRad += 20;
+    //     startRad += deltaRad*2;
     // }
 
-    for (let i = -10 + startRad; i <= width / 2; i += 10) {
-        let b = map(i, -10, width / 2, 50, 200);
+    for (let i = -deltaRad + startRad; i <= (width / 2)-(startRad/2); i += deltaRad) {
+        //for (let i = -deltaRad + startRad; i <= width ; i += deltaRad) {
+        let b = map(i, -deltaRad, width / 2, 50, 200);
         stroke(b, b, 255);
-        arc(i, height / 2, startRad, startRad, startAng, endAng);
+        arc(i, height / 2, rad, rad, startAng, endAng);
 
-        b = map(i, width / 2, -10, 50, 200);
+        b = map(i, width / 2, -deltaRad, 50, 200);
         stroke(b, b, 255);
-        arc(width - i, height / 2, startRad, startRad, endAng, startAng);
-        startRad += 20;
+        arc(width - i, height / 2, rad, rad, endAng, startAng);
+        rad += deltaRad * 2;
     }
 }
 
