9fb1bb6178ddfe84a80acafa757fe9e4ad16eb17
Crashnorun <charlie.portelli@gmail.com>
2018-06-10 19:35:47 -0400

    ading fill color

--- P5/Hexigons_02/cls_Hexigon.js
+++ P5/Hexigons_02/cls_Hexigon.js
@@ -1,15 +1,15 @@
 
 class Hexigon {
-/** @param {@param number X Location} locX 
- * @param {@param number Y Location} LocY 
- * @param {@param number Radius} Rad 
- */
+    /** @param {@param number X Location} locX 
+     * @param {@param number Y Location} LocY 
+     * @param {@param number Radius} Rad 
+     */
     constructor(locX, locY, Rad) {
         this.locX = locX;
         this.locY = locY;
         this.rad = Rad;
         this.col = [50, 50, 255];
-        this.fillCol = [255,0,0];
+        this.fillCol = [0, 0, 0];
         this.pts = [];
         this.calcPts();
     }
@@ -18,17 +18,22 @@ class Hexigon {
         for (let i = 0; i < PI * 2; i += PI / 3) {
             let x = (cos(i) * this.rad) + this.locX;
             let y = (sin(i) * this.rad) + this.locY;
-            let vect = new createVector(x,y,0);
+            let vect = new createVector(x, y, 0);
             this.pts.push(vect);
         }
     }
 
     render() {
-        noFill();
-       // fill(this.fillCol);
+        //noFill();
+        fill(this.fillCol);
         strokeWeight(3);
         stroke(this.col);
-        ellipse(this.locX, this.locY, this.rad, this.rad);
+        beginShape();
+        for (let i = 0; i < this.pts.length; i++) {
+            vertex(this.pts[i].x, this.pts[i].y);
+        }
+        endShape(CLOSE);
+        //ellipse(this.locX, this.locY, this.rad, this.rad);
     }
 
 }
--- P5/Hexigons_02/main.js
+++ P5/Hexigons_02/main.js
@@ -1,30 +1,64 @@
 ///<reference path = "C:\Users\Charlie\Documents\GitHub\Coding_Sketchbook\P5\addons\TypeScript Files\p5.global-mode.d.ts" />
 
 let hexigons = [];
-let rad = 50;
+let rad = 40;
+let centerLine;
+let tempCol = [];
 
 function setup() {
     createCanvas(600, 600);
     background(230);
+    centerLine = calcTriangleLength();
     let count = 0;
 
-    for (let j = 0; j <= height; j += rad) {
+    for (let j = 0; j <= height + centerLine; j += centerLine) {
         let tempHex = [];
-        for (let i = 0; i <= width; i += rad * 2) {
+        for (let i = 0; i <= width + centerLine; i += rad * 3) {
             let hex;
             if (count % 2 == 0) {
                 hex = new Hexigon(i, j, rad);
             } else {
-                hex = new Hexigon(i + rad / 2, j, rad);
+                hex = new Hexigon(i + (rad * 1.5), j, rad);
             }
-            hexigons.push(hex);
-            hex.render();
+            tempHex.push(hex);
+            // hex.render();
         }
-        count++;
         hexigons.push(tempHex);
+        count++;
     }
+    // console.log(hexigons.length);
+    // console.log(hexigons[0].length);
+    // console.log(hexigons);
 }
 
 function draw() {
 
+    for (let i = 0; i < hexigons.length; i++) {
+        for (let j = 0; j < hexigons[i].length; j++) {
+
+            if (abs(mouseX - hexigons[i][j].locX) < centerLine &&
+                abs(mouseY - hexigons[i][j].locY) < centerLine) {
+                    hexigons[i][j].fillCol = [255,0,0];
+            } else{
+                hexigons[i][j].fillCol = [0,0,255];
+            }
+            hexigons[i][j].render();
+        }
+    }
+}
+
+/** Each hexigon contains 6 equilateral triangles
+ * The length of each of the triangle sides = radius
+ * Must calculate the length of the center line of the triangle
+ */
+function calcTriangleLength() {
+    let degrees = (PI / 180) * 30;
+    let adjacent = cos(degrees) * rad;
+    console.log(adjacent);
+    return adjacent;
+    //cos(30) = adjacent / rad;
+    //30, 60, 90
+    //soh
+    //cah
+    //toa
 }
