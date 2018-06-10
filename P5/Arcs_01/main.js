e63c9cf9a3a98d98142aa738de95b500847debbe
Crashnorun <charlie.portelli@gmail.com>
2018-06-10 11:41:06 -0400

    Added design options.

--- P5/Arcs_01/main.js
+++ P5/Arcs_01/main.js
@@ -11,25 +11,27 @@ function setup() {
 
     let startAng = PI;
     let endAng = PI * 2;
-    let startRad =50;
+    let startRad = 10;
     let deltaRad = 2;
     let rad = startRad;
 
-    // for (let i = -startRad + startRad; i < 300; i += deltaRad) {
+    // for (let i = -deltaRad + startRad; i < 300; i += deltaRad) {
     //     let b = map(i, -deltaRad, width / 2, 50, 200);
     //     stroke(b, b, 255);
-    //     arc(i, height / 2, startRad, startRad, startAng, endAng);
-
-    //     b = map(i, width / 2, -deltaRad, 50, 200);
-    //     stroke(b, b, 255);
-    //     arc(width / 2 - i, height / 2, startRad - 600, startRad - 600, endAng, startAng);
-    //     startRad += deltaRad*2;
+    //     translate(width / 2, height / 2);
+    //     rotate(map(i, -deltaRad, width / 2, 0, PI));
+    //     translate(-width / 2, -height / 2);
+    //     ellipse(i, height / 2, rad, rad);
+    //     rad += deltaRad * 2;
     // }
 
-    for (let i = -deltaRad + startRad; i <= (width / 2)-(startRad/2); i += deltaRad) {
+    for (let i = -deltaRad + startRad; i <= (width / 2) - (startRad / 2); i += deltaRad) {
         //for (let i = -deltaRad + startRad; i <= width ; i += deltaRad) {
         let b = map(i, -deltaRad, width / 2, 50, 200);
         stroke(b, b, 255);
+        // translate(width / 2, height / 2);
+        // rotate(map(i, -deltaRad, width / 2, 0, PI));
+        // translate(-width / 2, -height / 2);
         arc(i, height / 2, rad, rad, startAng, endAng);
 
         b = map(i, width / 2, -deltaRad, 50, 200);
