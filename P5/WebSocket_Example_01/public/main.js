dba1ad0e82b38c33e5088cee54e637e3c82e176d
Crashnorun <charlie.portelli@gmail.com>
2018-08-18 20:43:23 -0400

    set up basic sever code that works

--- P5/WebSocket_Example_01/public/main.js
+++ P5/WebSocket_Example_01/public/main.js
@@ -1,9 +1,11 @@
 ///<reference path = "C:\Users\Charlie\Documents\GitHub\Coding_Sketchbook\P5\addons\TypeScript Files\p5.global-mode.d.ts" />
 
-function setup(){
-
+function setup() {
+    createCanvas(600, 400);
+    background(0);
 }
 
-function draw(){
-
+function draw() {
+    background(51);
+    ellipse(mouseX, mouseY, 20, 20);
 }
--- P5/WebSocket_Example_01/server.js
+++ P5/WebSocket_Example_01/server.js
@@ -1,2 +1,15 @@
 
+
+// import express library
+var express = require("express");
+
+// crete an express application
+var app = express();
+
+// create a port to listen to
+var server = app.listen(3000);
+
+// make the files in the 'public' folder available
+app.use(express.static("public"));
+
 console.log("Socket server is running");
