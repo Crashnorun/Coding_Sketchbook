041373b41bbd6da76929f9d33342145bdf2ff30f
Crashnorun <charlie.portelli@gmail.com>
2018-09-30 20:47:08 -0400

    adding short csv file.

--- D3/BarChart_02/main.js
+++ D3/BarChart_02/main.js
@@ -1,6 +1,6 @@
 
 
-let path = "201808-citibike-tripdata/201808-citibike-tripdata.csv";
+let path = "201808-citibike-tripdata/Short_File_01.csv";
 
 let data = d3.csv(path, function (data){ 
     console.log(data); 
