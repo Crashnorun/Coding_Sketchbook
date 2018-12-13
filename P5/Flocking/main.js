31e5143847e1fd2f48080ca7ae90a05069c60aac
crashnorun <charlie.portelli@gmail.com>
2018-12-12 20:10:51 -0500

    Adding Flocking example

---
+++ P5/Flocking/main.js
@@ -0,0 +1,27 @@
+///<reference path = "C:\Users\cportelli\Documents\Personal\GitHub\Coding_Sketchbook\P5\addons\TypeScript Files\p5.global-mode.d.ts" />
+/*
+References: https://www.red3d.com/cwr/boids/
+https://www.youtube.com/redirect?v=mhjuuHl6qHM&event=video_description&redir_token=PBOnuuqSWQw9MXXckwGAf2GAuvJ8MTU0NDc0NzI1MEAxNTQ0NjYwODUw&q=https%3A%2F%2Fnatureofcode.com%2Fbook%2Fchapter-6-autonomous-agents%2F
+*/
+
+const flock = [];
+
+function setup() {
+    createCanvas(640, 360);
+    for(let i = 0; i < 100; i++){
+        flock.push(new Particle());
+    }
+
+}
+
+function draw() {
+    background(51);
+
+    for (let particle of flock) {
+        particle.edges();
+        particle.steer(flock);
+        //particle.align(flock);
+        particle.update();
+        particle.show();
+    }
+}
---
+++ P5/Flocking/particle.js
@@ -0,0 +1,67 @@
+
+class Particle {
+
+    constructor() {
+        this.Position = createVector(random(width), random(height));
+        this.Velocity = p5.Vector.random2D();
+        this.Velocity.setMag(random(0.5, 1.5));          // random magnitude
+        this.Acceleration = createVector();
+        this.MaxForce = 0.2;
+        this.MaxSpeed = 1;
+    }
+
+    edges() {
+        if (this.Position.x > width) {
+            this.Position.x = 0;
+        } else if (this.Position.x < 0) {
+            this.Position.x = width;
+        }
+
+        if (this.Position.y > height) {
+            this.Position.y = 0;
+        } else if (this.Position.y < 0) {
+            this.Position.y = height;
+        }
+    }
+
+    align(flock) {
+        let avg = createVector();
+        let maxDist = 50;
+        let total = 0;
+        for (let particle of flock) {
+            let d = dist(this.Position.x, this.Position.y, particle.Position.x, particle.Position.y);
+            if (d < maxDist && particle != this) {
+                avg.add(particle.Velocity);
+                total++;
+            }
+        }
+        if (total > 0) {
+            avg = avg.div(total);
+            avg.setMag(this.MaxSpeed);
+            avg.sub(this.Velocity);
+            avg.limit(this.MaxForce);
+        }
+        return avg;
+    }
+
+    steer(flock) {
+        let alignment = this.align(flock);
+        this.Acceleration = alignment;
+    }
+
+    update() {
+        this.Position.add(this.Velocity);
+        this.Velocity.add(this.Acceleration);
+    }
+
+
+    show() {
+
+        strokeWeight(8);
+        stroke(255);
+        point(this.Position.x, this.Position.y);
+    }
+
+
+
+}
