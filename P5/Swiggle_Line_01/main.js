4e11a20762780a84fee0547ee90efc3bb5b17fa3
Crashnorun <charlie.portelli@gmail.com>
2019-03-02 18:50:12 -0500

    happy mistake

--- P5/Swiggle_Line_01/main.js
+++ P5/Swiggle_Line_01/main.js
@@ -11,9 +11,10 @@ let pts = [];
 function setup() {
     createCanvas(600, 600);
     background(0);
+    frameRate(10);
 
-    let ptPrev = new Point(createVector(random(1, 5), random(1, 5)), 5);
-    ptPrev.vect.setMag(80);
+    let ptPrev = new Point(createVector(random(100, 150), random(100, 150)), 5);
+    ptPrev.vect.setMag(200);
     ptPrev.offset = 0.01;
     ptPrev.color = [random(0, 255), random(0, 255), random(200, 255)];
     pts.push(ptPrev);
@@ -41,6 +42,19 @@ function setup() {
 }
 
 function draw() {
+    //background(0, 0.1);
+    fill(0,15);
+    rect(0, 0, width, height);
+    translate(width / 2, height / 2);
+    for (let i = 0; i < pts.length; i++) {
+        stroke(pts[i].color);
+        strokeWeight(pts[i].weight);
+        ellipse(pts[i].vect.x, pts[i].vect.y, pts[i].weight, pts[i].weight);
+        if (i > 0)
+            line(pts[i - 1].vect.x, pts[i - 1].vect.y, pts[i].vect.x, pts[i].vect.y)
+        pts[i].offset += 0.001;
+        pts[i].rotateVector();
+    }
 
 }
 
