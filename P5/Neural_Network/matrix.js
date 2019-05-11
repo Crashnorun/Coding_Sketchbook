721fcdebd350a9ee4f034e9be3e06fb0cc404b10
Crashnorun <charlie.portelli@gmail.com>
2019-05-11 11:40:00 -0400

    added matrix class

--- P5/Neural_Network/matrix.js
+++ P5/Neural_Network/matrix.js
@@ -1,68 +1,67 @@
 
+class Matrix {
+    //#region CONSTRUCTOR
+    construtor(rows, cols) {
 
-//#region CONSTRUCTOR
-function Matrix(rows, cols) {
-
-    this.rows = rows;
-    this.cols = cols;
-    this.matrix = [];
-    for (let i = 0; i < this.rows; i++) {
-        this.matrix[i] = [];
-
-        for (let j = 0; j < this.cols; j++) {
-            this.matrix[i][j] = 0;
-        }
-    }
-}
-//#endregion
-
-
-Matrix.prototype.randomize = function () {
-    for (let i = 0; i < this.rows; i++) {
-        for (let j = 0; j < this.cols; j++) {
-            this.matrix[i][j] = Math.floor(Math.random() * 10);
-        }
-    }
-}
-
-
-
-//#region  SCALAR OPERATIONS
-
-Matrix.prototype.multiply = function (n) {
-
-    if (n instanceof Matrix) {      // element wise multiplication
+        this.rows = rows;
+        this.cols = cols;
+        this.matrix = [];
         for (let i = 0; i < this.rows; i++) {
+            this.matrix[i] = [];
+
             for (let j = 0; j < this.cols; j++) {
-                this.matrix[i][j] *= n.matrix[i][j];
-            }
-        }
-    } else {    // scalar function that multiplies a matrix
-        for (let i = 0; i < this.rows; i++) {
-            for (let j = 0; j < this.cols; j++) {
-                this.matrix[i][j] *= n;
+                this.matrix[i][j] = 0;
             }
         }
     }
-}
+    //#endregion
 
 
-Matrix.prototype.add = function (n) {
-
-    if (n instanceof Matrix) {  // element wise addition
+    randomize() {
         for (let i = 0; i < this.rows; i++) {
             for (let j = 0; j < this.cols; j++) {
-                this.matrix[i][j] += n.matrix[i][j];
-            }
-        }
-    } else {    // scalar function that adds a matrix
-        for (let i = 0; i < this.rows; i++) {
-            for (let j = 0; j < this.cols; j++) {
-                this.matrix[i][j] += n;
+                this.matrix[i][j] = Math.floor(Math.random() * 10);
             }
         }
     }
+
+
+    //#region  
+
+    multiply(n) {
+
+        if (n instanceof Matrix) {      // element wise multiplication
+            for (let i = 0; i < this.rows; i++) {
+                for (let j = 0; j < this.cols; j++) {
+                    this.matrix[i][j] *= n.matrix[i][j];
+                }
+            }
+        } else {    // scalar function that multiplies a matrix
+            for (let i = 0; i < this.rows; i++) {
+                for (let j = 0; j < this.cols; j++) {
+                    this.matrix[i][j] *= n;
+                }
+            }
+        }
+    }
+
+
+    add(n) {
+
+        if (n instanceof Matrix) {  // element wise addition
+            for (let i = 0; i < this.rows; i++) {
+                for (let j = 0; j < this.cols; j++) {
+                    this.matrix[i][j] += n.matrix[i][j];
+                }
+            }
+        } else {    // scalar function that adds a matrix
+            for (let i = 0; i < this.rows; i++) {
+                for (let j = 0; j < this.cols; j++) {
+                    this.matrix[i][j] += n;
+                }
+            }
+        }
+    }
+    //#endregion
+
 }
-//#endregion
-
-
