c59f0fda336f7a4f4c476fe7dcdfe3bd1e392d22
Crashnorun <charlie.portelli@gmail.com>
2019-05-11 19:57:10 -0400

    material stuff

---
+++ P5/Materials/main.js
@@ -0,0 +1,17 @@
+///<reference path = "C:\Users\Charlie\Documents\GitHub\Coding_Sketchbook\P5\addons\TypeScript Files\p5.global-mode.d.ts" />
+
+let img;
+
+function preload(){
+    img = loadImage("Map_01.png");
+}
+
+function setup() {
+    createCanvas(600, 600, WEBGL);
+    background(0);
+sphere(200)
+}
+
+function draw() {
+
+}
