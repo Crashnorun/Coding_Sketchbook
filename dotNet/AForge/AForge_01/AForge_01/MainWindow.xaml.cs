1949067a4d28b71f2b22dbda05013e70b2916534
Crashnorun <charlie.portelli@gmail.com>
2019-08-17 00:59:34 -0400

    added slider

--- dotNet/AForge/AForge_01/AForge_01/MainWindow.xaml.cs
+++ dotNet/AForge/AForge_01/AForge_01/MainWindow.xaml.cs
@@ -6,9 +6,11 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
@@ -27,34 +29,89 @@ namespace AForge_01
     /// </summary>
     public partial class MainWindow : Window
     {
-        public string filePath = @"C:\Users\Charlie\Pictures\IMG_6517_A.jpg";                      // image path
+        public string filePath = @"C:\Users\Charlie\Pictures\IMG_6517_A.jpg";                   // image path
+        private bool dragStarted = false;
+        private Grayscale filter;
+        private string tempPath;
+        string tempFile;
 
         public MainWindow()
         {
             InitializeComponent();
-            ImgBox1.Source = new BitmapImage(new Uri(filePath, UriKind.Relative));              // original image
-
+            //ImgBox1.Source = new BitmapImage(new Uri(filePath, UriKind.Relative));              // original image
+            filterScale.Visibility = Visibility.Hidden;
+            tempPath = System.IO.Path.GetTempPath();                                            // get temp path
+            tempFile = System.IO.Path.Combine(tempPath, "tempImg.jpg");                         // get full temp path
         }
 
         private void btnProcess_Click(object sender, RoutedEventArgs e)
         {
-
-            string tempPath = System.IO.Path.GetTempPath();                                     // get temp path
-
-            string tempFile = System.IO.Path.Combine(tempPath, "tempImg.jpg");                  // get full temp path
-            // System.IO.File.Delete(tempFile);                                                    // delete the old file
-
-            Grayscale filter = new Grayscale(0.2, 0.2, 0.2);                                    // create grey filter
-            Bitmap img = (Bitmap)System.Drawing.Image.FromFile(filePath);                       // get image 
-            Bitmap grayImage = filter.Apply(img);                                               // apply filter
-            grayImage.Save(tempFile);                                                           // save new image
-
+            applyFilter(0.2);
             ImgBox1.Source = new BitmapImage(new Uri(filePath));                                // original image
-            ImgBox2.Source = new BitmapImage(new Uri(tempFile));                                // new image
             OriginalName.Text = System.IO.Path.GetFileName(filePath);
             TempName.Text = System.IO.Path.GetFileName(tempFile);
+            filterScale.Visibility = Visibility.Visible;
+            filterScale.Value = 0.2;
         }
 
+        //private void Slider_DragStarted(object sender, DragStartedEventArgs e)
+        //{
+        //    this.dragStarted = true;
+        //}
+
+        //private void filterScale_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        //{
+        //    if (!dragStarted)
+        //        applyFilter(e.NewValue);
+        //}
+
+        private void applyFilter(double filterValue)
+        {
+            if (System.IO.File.Exists(tempFile))
+            {
+                ImgBox2.Source = null;
+                System.IO.File.Delete(tempFile);                                                // delete the old file
+            }
+
+            filter = new Grayscale(filterValue, filterValue, filterValue);                      // create grey filter
+            Bitmap img = (Bitmap)System.Drawing.Image.FromFile(filePath);                       // get image 
+            Bitmap grayImage = filter.Apply(img);                                               // apply filter
+                                                                                                //grayImage.Save(tempFile);                                                           // save new image
+                                                                                                //ImgBox2.Source = new BitmapImage(new Uri(tempFile));                                // new image
+            IntPtr hbitmap = grayImage.GetHbitmap();
+            ImgBox2.Source = Imaging.CreateBitmapSourceFromHBitmap(hbitmap, IntPtr.Zero, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions());
+
+        }
+
+        private void Slider_DragCompleted(object sender, DragCompletedEventArgs e)
+        {
+            //System.Diagnostics.Debug.Print(((Slider)sender).Value.ToString());
+            applyFilter(((Slider)sender).Value);
+            this.dragStarted = false;
+        }
+
+        private void filterScale_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            System.Diagnostics.Debug.Print(((Slider)sender).Value.ToString());
+            applyFilter(((Slider)sender).Value);
+        }
+
+
+        private void filterScale_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            System.Diagnostics.Debug.Print(((Slider)sender).Value.ToString());
+            applyFilter(((Slider)sender).Value);
+        }
+
+        private void filterScale_OnThumbDragStarted(DragStartedEventArgs e)
+        {
+
+        }
+
+        private void filterScale_DragStarted(object sender, DragStartedEventArgs e)
+        {
+
+        }
     }
 
 }
