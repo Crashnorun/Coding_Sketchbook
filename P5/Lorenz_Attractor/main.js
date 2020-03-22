d193f152bb81ff86091b177251fd383730441537
crashnorun <charlie.portelli@gmail.com>
2020-03-22 12:31:58 -0400

    Figuring out camera location

--- P5/Lorenz_Attractor/main.js
+++ P5/Lorenz_Attractor/main.js
@@ -10,37 +10,50 @@ let rho = 28;
 let sigma = 10;
 let x, y, z;
 let t = 0.02445;
+let cam;
 
 function setup() {
     createCanvas(600, 600, WEBGL);
     background(0);
     stroke('purple');
     strokeWeight(1);
+    fill('red');
+
     x = 1;
     y = 0;
     z = 0;
 
-    camera(07,-109,111,1.5, -60,73);
+    //camera(07, -109, 111, 1.5, -60, 73);
+    //camera(8, -60, 25, 8, 5, 25);
+   // camera(0, 0, 600, 0, 0, 0, 0, 0, 1);
+   cam = camera();
+   console.log(cam);
     console.log("exit setup");
 }
 
 
 
 function draw() {
-orbitControl();
+    /*orbitControl();
     beginShape(POINTS);
-    
+
     let xt = x + t * sigma * (y - x);
     let yt = y + t * (x * (rho - z) - y);
     let zt = z + t * (x * y - beta * z);
 
-    vertex(x,y,z);
-    
+    vertex(x, y, z);
+
     //point(xt, yt, zt);
     x = xt;
     y = yt;
     z = zt;
 
-    endShape();
+    endShape();*/
+
+    // background(200);
+
+    rotateX(frameCount * 0.01);
+    rotateY(frameCount * 0.01);
+    box(150);
 
 }
