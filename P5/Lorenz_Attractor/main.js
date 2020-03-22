ae25490445e9a8ccfb3677e782b71a7ca5705ed4
crashnorun <charlie.portelli@gmail.com>
2020-03-22 14:24:59 -0400

    Figuring out P5 camera

--- P5/Lorenz_Attractor/main.js
+++ P5/Lorenz_Attractor/main.js
@@ -11,49 +11,58 @@ let sigma = 10;
 let x, y, z;
 let t = 0.02445;
 let cam;
+let slideX, slideY, slideZ;
 
 function setup() {
     createCanvas(600, 600, WEBGL);
     background(0);
     stroke('purple');
-    strokeWeight(1);
+    strokeWeight(4);
+    noStroke();
     fill('red');
 
-    x = 1;
+    x = 0.1;
     y = 0;
     z = 0;
 
+    slideX = createSlider(-100, 100, 0, 1);
+    slideY = createSlider(-100, 100, 0, 1);
+    slideZ = createSlider(-100, 100, 100, 1);
+
     //camera(07, -109, 111, 1.5, -60, 73);
-    //camera(8, -60, 25, 8, 5, 25);
-   // camera(0, 0, 600, 0, 0, 0, 0, 0, 1);
-   cam = camera();
-   console.log(cam);
+    // camera(8, -60, 25, 8, 5, 25);
+    // camera(slideX.value(), 0, 100, 0, 0, 0, 0, 0, 1);
+    camera(0, 0, 100, 0, 0, 0, 0, 0, 1);
+    // cam = camera();
+    console.log(cam);
     console.log("exit setup");
 }
 
 
 
 function draw() {
-    /*orbitControl();
-    beginShape(POINTS);
-
+    orbitControl();
+    // beginShape(LINES);
+    // camera(slideX.value(), slideY.value(), slideZ.value(), 0, 0, 0, 0, 0, 1);
     let xt = x + t * sigma * (y - x);
     let yt = y + t * (x * (rho - z) - y);
     let zt = z + t * (x * y - beta * z);
 
-    vertex(x, y, z);
+     push();
+     translate(xt, yt, zt);
+     sphere(0.25);
+     pop();
 
+    // vertex(x, y, z);
     //point(xt, yt, zt);
     x = xt;
     y = yt;
     z = zt;
 
-    endShape();*/
-
+    //  endShape();
     // background(200);
 
-    rotateX(frameCount * 0.01);
-    rotateY(frameCount * 0.01);
-    box(150);
-
+    // rotateX(frameCount * 0.01);
+    // rotateY(frameCount * 0.01);
+    // box(150);
 }
