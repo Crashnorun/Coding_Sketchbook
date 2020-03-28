c3e513918fa251fe38475c79c98903117956c391
crashnorun <charlie.portelli@gmail.com>
2020-03-28 17:44:06 -0400

    Initial Verb setup

---
+++ Verb.js/CurveByPointInterpolation/main.js
@@ -0,0 +1,15 @@
+
+/*
+ setupScene();
+
+ var p1 = [-10, 0, 0], p2 = [10, 0, 0], p3 = [10, 10, 0], p4 = [0, 10, 0], p5 = [5, 5, 0];
+ var pts = [p1, p2, p3, p4, p5];
+ var interpCurve = verb.geom.NurbsCurve.byPoints(pts, 3);
+
+ addCurveToScene(interpCurve.toThreeGeometry());
+
+ addPointsToScene(pts);
+
+ renderScene();*/
+
+ console.log("Charlie");
