7863feae44d20264ff04aecda8dcac33be39bab6
crashnorun <charlie.portelli@gmail.com>
2020-05-15 17:56:48 -0400

    Update main.js

--- P5/Circle_Packing/main.js
+++ P5/Circle_Packing/main.js
@@ -6,13 +6,16 @@
 
 let circs = [];
 let totalMove = [];
-const numpts = 500;
+const numpts = 300;
 const maxRad = 50;
+let col;
 
 function setup() {
 
     createCanvas(600, 600);
     background(0);
+    col = color(0, 10);
+    frameRate(10);
 
     for (let i = 0; i < numpts; i++) {
         let radius = random(0, maxRad);
@@ -27,12 +30,17 @@ function setup() {
 }
 
 
-
-
 function draw() {
 
-background(0);
+    //background(0);
+    fill(col);
+    rect(0, 0, width, height);
+
     let totalCollisions = [];
+    // for (let i = 0; i < circs.length; i++) {
+    //     circs[i].numCollisions = 0;
+    // }
+
 
     for (let i = 0; i < circs.length; i++) {
         for (let j = i + 1; j < circs.length; j++) {
@@ -43,10 +51,27 @@ background(0);
 
             let vect = p5.Vector.sub(circs[i].cent, circs[j].cent);
             vect.normalize();
-            vect = vect.mult(0.5 * (circs[i].radius + circs[j].radius) - d);
+            vect = vect.mult(0.5 * ((circs[i].radius + circs[j].radius) - d));
 
-            totalMove[i] = p5.Vector.add(totalMove[i], vect);
-            totalMove[j] = p5.Vector.add(totalMove[j], vect);
+            // option 01
+            // totalMove[i] = p5.Vector.sub(totalMove[i], vect);
+            // totalMove[j] = p5.Vector.add(totalMove[j], vect);
+
+            // option 02
+             totalMove[i] = p5.Vector.add(totalMove[i], vect);
+             totalMove[j] = p5.Vector.sub(totalMove[j], vect);
+
+            // option 03
+            // totalMove[i] = p5.Vector.add(totalMove[i], vect);
+            // totalMove[j] = p5.Vector.add(totalMove[j], vect);
+
+            // option 04
+            //  totalMove[i] = p5.Vector.sub(totalMove[i], vect);
+            //  totalMove[j] = p5.Vector.sub(totalMove[j], vect);
+
+            // option 05
+            // totalMove[i] = p5.Vector.sub(totalMove[i], vect);
+            // totalMove[j] = p5.Vector.sub(totalMove[j], vect);
 
             circs[i].numCollisions++;
             circs[j].numCollisions++;
@@ -59,8 +84,13 @@ background(0);
         if (circs[i].numCollisions > 0) {
             let averageVect = p5.Vector.div(totalMove[i], circs[i].numCollisions);
             circs[i].cent = p5.Vector.add(circs[i].cent, averageVect);
+
+            if (circs[i].cent.x >= width || circs[i].cent.x <= 0) circs[i].cent.x *= -1;
+            if (circs[i].cent.y >= height || circs[i].cent.y <= 0) circs[i].cent.y *= -1;
+
             circs[i].Render();
             circs[i].numCollisions = 0;
+            totalMove[i] = createVector(0, 0);
         }
     }
 }
