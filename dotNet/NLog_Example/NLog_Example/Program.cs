763627541613c0ae4aaa4d7442ac11fc6af6ff76
crashnorun <charlie.portelli@gmail.com>
2020-07-14 08:06:59 -0400

    added invokeMethod

--- dotNet/NLog_Example/NLog_Example/Program.cs
+++ dotNet/NLog_Example/NLog_Example/Program.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace NLog_Example
 {
-    class Program
+    public class Program
     {
 
         public static NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
@@ -15,27 +16,65 @@ namespace NLog_Example
         {
             logger.Debug("Debugging");
             logger.Error("Erroring");
-            logger.Debug(new Exception("hello exception"), "Dbuging exception");
+            logger.Debug(new Exception("hello exception"), "Debuging exception");
+
+            MathFunctions.Add(1, 1.0);
+
+            MathFunctions.Divide(0, 0);
+
+            MathFunctions.Subtract(10, 20);
+
+            MathFunctions.Add(new List<int> { 12, 12 });
+
 
             string URL = "https://docs.google.com/spreadsheets/d/1bB4BzHTexDvW53PDTlX9nFqHLznicL7YqXP8ACFqJIs/edit#gid=0";
             Uri uriResult;
             bool result = Uri.TryCreate(URL, UriKind.Absolute, out uriResult) &&
                 (uriResult.Scheme == Uri.UriSchemeHttp || uriResult.Scheme == Uri.UriSchemeHttps);
 
-            if (!result) Console.WriteLine("URL input is not valid");
+            if (!result)
+            {
+                Console.WriteLine("URL input is not valid");
+                logger.Error("URL input is not valid");
+            }
 
             // parse url for sheet ID
             string[] vals = new Uri(URL).Segments;
 
             string SheetID = string.Empty;
 
-            foreach(string str in vals)
+            foreach (string str in vals)
             {
                 if (str.Length > SheetID.Length) SheetID = str;
             }
             SheetID = SheetID.Remove(SheetID.Length - 1, 1);
-            
 
+            Console.ReadKey();
         }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="Method"></param>
+        /// <param name="ParamValues"></param>
+        /// <References>https://forums.asp.net/t/1421332.aspx?How+to+get+current+method+parameter+values+programatically+
+        /// https://docs.microsoft.com/en-us/dotnet/api/system.reflection.methodbase.invoke?view=netcore-3.1
+        /// </References>
+        public static void InvokeMethod(MethodBase Method, params object[] ParamValues)
+        {
+            // get the method name
+            logger.Debug("Method name: " + Method.Name);
+
+            // get the method parameters
+            ParameterInfo[] pars = Method.GetParameters();
+
+            // print the parameter names and their values
+            for (int i = 0; i < ParamValues.Length; i++)
+                logger.Debug(string.Format("\t Parameter: {0}, Value: {1}", pars[i].Name, ParamValues[i]));
+
+            MethodInfo methodInfo = (MethodInfo)Method;
+        }
+
     }
 }
